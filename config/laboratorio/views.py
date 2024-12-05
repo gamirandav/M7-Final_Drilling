aa76c9d431f66da2167b7f8bb964519b24a35032
Gmo. Miranda <gmo.mirandav@gmail.com>
2024-12-05 20:24:33 -0300

    Segundo commit: M7-Final_Drilling

--- config/laboratorio/views.py
+++ config/laboratorio/views.py
@@ -8,33 +8,18 @@ def listar_laboratorios(request):
     return render(request, 'listar.html', {'laboratorios': laboratorios})
 
 
-# def agregar_laboratorio(request):
-#    if request.method == 'POST':
-#        form = LaboratorioForm(request.POST)
-#        if form.is_valid():
-#            form.save()
-#            return redirect('listar_laboratorios')
-#    else:
-#        form = LaboratorioForm()
-#    return render(request, 'form.html', {'form': form})
-
-
 def agregar_laboratorio(request, laboratorio_id=None):
-    # Si se proporciona un ID, obtén el laboratorio existente
+
     laboratorio = get_object_or_404(
         Laboratorio, id=laboratorio_id) if laboratorio_id else None
 
     if request.method == 'POST':
-        # Procesar datos enviados en el formulario
         form = LaboratorioForm(request.POST, instance=laboratorio)
         if form.is_valid():
-            form.save()  # Guarda el laboratorio
-            # Redirigir a la lista de laboratorios
+            form.save()
             return redirect('listar_laboratorios')
     else:
-        # Mostrar formulario vacío o prellenado para editar
         form = LaboratorioForm(instance=laboratorio)
-
     return render(request, 'form.html', {'form': form, 'laboratorio': laboratorio})
 
 
